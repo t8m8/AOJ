93df955b3772f71ac1235f28989ae5c4786d6fd5
t8m8 <t0m09488@gmail.com>
2015-09-25 21:24:24 +0900

    add 0013

---
+++ Volume0/AOJ0013.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.awt.geom.*;
+import java.math.*;
+
+public class AOJ0013 {
+
+	static final Scanner in = new Scanner(System.in);
+	static final PrintWriter out = new PrintWriter(System.out,false);
+
+	static void solve() {
+
+		ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
+
+		while (in.hasNext()) {
+			int x = in.nextInt();
+			if (x != 0) stack.addLast(x);
+			else out.println(stack.pollLast());
+			out.flush();
+		}
+	}
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+
+		solve();
+		out.flush();
+
+		long end = System.currentTimeMillis();
+		//trace(end-start + "ms");
+		in.close();
+		out.close();
+	}
+
+	static void trace(Object... o) { System.out.println(Arrays.deepToString(o));}
+}
