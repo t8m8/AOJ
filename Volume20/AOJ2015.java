7399b0405cd0c31d6cc1960dcc438fe7591a15c9
t8m8 <t0m09488@gmail.com>
2015-12-23 17:18:33 +0900

    fix 2015

--- Volume20/AOJ2015.java
+++ Volume20/AOJ2015.java
@@ -8,33 +8,50 @@ public class AOJ2015 {
 	static final Scanner in = new Scanner(System.in);
 	static final PrintWriter out = new PrintWriter(System.out,false);
 
+	static int[] cnth = new int[1500001];
+	static int[] cntw = new int[1500001];
+
 	static boolean solve() {
 		int n = in.nextInt();
 		int m = in.nextInt();
 		if (n + m == 0) return false;
 
-		int[] h = new int[n];
-		int[] w = new int[m];
-		for (int i=0; i<n; i++) {
+		int[] h = new int[n+1];
+		for (int i=1; i<=n; i++) {
 			h[i] = in.nextInt();
+			h[i] += h[i-1];
 		}
-		for (int i=0; i<m; i++) {
+
+		int[] w = new int[m+1];
+		for (int i=1; i<=m; i++) {
 			w[i] = in.nextInt();
+			w[i] += w[i-1];
 		}
 
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m; j++) {
+		Arrays.fill(cnth,0);
 
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=n; j++) {
+				if (i-j < 0) continue;
+				cnth[h[i] - h[i-j]]++;
 			}
 		}
 
-		int[][] dp = new int[n][m];
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m; j++) {
+		Arrays.fill(cntw,0);
 
+		for (int i=1; i<=m; i++) {
+			for (int j=1; j<=m; j++) {
+				if (i-j < 0) continue;
+				cntw[w[i] - w[i-j]]++;
 			}
 		}
 
+		int ans = 0;
+		for (int i=0; i<1500001; i++) {
+			ans += cnth[i] * cntw[i];
+		}
+
+		out.println(ans);
 
 		return true;
 	}
